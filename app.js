4d7a524d946a1e86a617a09242ad3784dca3de86
unknown <gokul03062002@gmail.com>
2022-01-15 20:35:52 +0530

    third

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express=require("express");
 const bodyparser=require("body-parser");
 const app=express();
+const currentdate=require(__dirname+"/date.js");
 const mongoose=require("mongoose");
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set("view engine","ejs");
 
- mongoose.connect("mongodb+srv://19csr044:Gokul2002@cluster0.dowfo.mongodb.net/Balamurugan-thirukovil");
+ mongoose.connect("mongodb://19csr044:Gokul2002@cluster0-shard-00-00.dowfo.mongodb.net:27017,cluster0-shard-00-01.dowfo.mongodb.net:27017,cluster0-shard-00-02.dowfo.mongodb.net:27017/Balamurugan-thirukovil?ssl=true&replicaSet=atlas-108jkb-shard-0&authSource=admin&retryWrites=true&w=majority");
  const detailschema={
      date:Number,
      detail:String,
@@ -30,18 +31,32 @@ const poojadetail3=new poojadetail({
 });
 const defaultarray=[poojadetail1,poojadetail2,poojadetail3];
 app.get("/",function(req,res){
-
+    var fullday=currentdate.getDate();
+    console.log(fullday);
+    var day=currentdate.getDay();
+    console.log(day);
+    
     poojadetail.find({},function(err,founddetail)
     {
+        poojadetail.find({date:day},function(err,details)
+        {
+            var name=details[0].detail;
+            res.render("poojadetail",{detail:founddetail,todaydetail:name,todaydate:fullday});
+        });
         //console.log(founddetail);
-        res.render("poojadetail",{detail:founddetail});
+       
+        
+
 
     });
   
 
 });
-app.get("/bookdetail",function(req,res){
-    res.render("booking",{});
+app.post("/bookdetail",function(req,res){
+    const date=req.body.book;
+    console.log(date);
+   
+    res.render("booking",{bookdate:date});
     
   
 
@@ -56,6 +71,7 @@ const booking=mongoose.model("booking",bookingschema);
 
 app.post("/booking",function(req,res)
 {
+    
     
     const date=req.body.date;
     const detail=req.body.detail;
@@ -89,9 +105,10 @@ app.get("/adminlist",function(req,res)
   
 
 });
-app.get("/adminadd",function(req,res)
+app.post("/adminupdate",function(req,res)
 {
-    res.render("adminadd",{});
+    const updatedate=req.body.updatedate;
+    res.render("adminadd",{date:updatedate});
 
 });
 app.post("/adminadd",function(req,res)
@@ -118,7 +135,66 @@ app.post("/adminadd",function(req,res)
     res.redirect("/adminlist");
 
 });
-app.listen(process.env.PORT || 3000,function()
+const adminschema={
+    username:String,
+    password:String
+};
+const admindetail=mongoose.model("adminlogin",adminschema);
+const admin=new admindetail({
+    username:"gokul",
+    password:"gokul2002"
+});
+// admin.save();
+app.get("/login",function(req,res)
+{
+    
+res.render("login",{});
+
+});
+
+app.post("/login",function(req,res)
+{
+    const uname=req.body.username;
+     var password=req.body.password;
+    console.log(uname);
+    console.log(password);
+    admindetail.find({username:uname},function(err,found)
+    {
+       console.log(err);
+      if(found.length===0)
+      {
+          res.redirect("/login");
+      }
+      else{
+          var orgpassword=found[0].password;
+        if(orgpassword===password)
+        {
+            res.redirect("/adminlist");
+        }
+        else{
+            res.redirect("/login");
+        }
+      }
+      
+
+    });
+  
+
+});
+app.get("/userlogin",function(req,res)
+{
+
+res.render("userlogin",{});
+
+
+});
+app.get("/adminlogin",function(req,res)
+{
+
+res.render("adminlogin",{});
+
+});
+app.listen(process.env.PORT || 5000,function()
 {
     console.log("server started");
 });
---
+++ date.js
@@ -0,0 +1,36 @@
+//console.log(module);
+module.exports.getDate=getDate; 
+
+function getDate()
+{
+    var today=new Date();
+    var currdate=today.getDay();
+   // console.log(currdate);
+    var days=["sunday","monday","tuesday","wednesday","friday","saturday"];
+    var day="";
+    var options ={
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+
+    };
+    day=today.toLocaleDateString("en-In",options);
+    return day;
+
+}
+module.exports.getDay=getDay; 
+function getDay()
+{
+    var today=new Date();
+    var currdate=today.getDay();
+   // console.log(currdate);
+    var days=["sunday","monday","tuesday","wednesday","friday","saturday"];
+    var day="";
+    var options ={
+        day:"numeric",
+
+    };
+    day=today.toLocaleDateString("en-In",options);
+    return day;
+
+}   

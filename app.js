32854d1219d13ead05724f6f814aac0dce9ba283
unknown <gokul03062002@gmail.com>
2022-02-18 20:42:18 +0530

    sasti

--- app.js
+++ app.js
@@ -14,22 +14,61 @@ app.set("view engine","ejs");
      status:String
  };
  const poojadetail=mongoose.model("poojadetail",detailschema);
- const poojadetail1=new poojadetail({
-     date:1,
+ const karthigaidetailschema={
+    date:String,
+    detail:String,
+    status:String,
+    tamilmonth:String
+};
+ const karthigaipoojadetail=mongoose.model("karthigaipoojadetail",karthigaidetailschema);
+ const karthigaipoojadetail1=new karthigaipoojadetail({
+     date:"03-06-2022",
      detail:"gokul and gokul mobiles",
-     status:"booked"
+     status:"booked",
+     tamilmonth:"sdnasj"
  });
- const poojadetail2=new poojadetail({
+ const karthigaipoojadetail2=new karthigaipoojadetail({
+    date:"03-06-2022",
+    detail:"gokul and gokul mobiles",
+    status:"booked",
+    tamilmonth:"sdnasj"
+});
+const karthigaipoojadetail3=new karthigaipoojadetail({
+    date:"03-06-2022",
+    detail:"gokul and gokul mobiles",
+    status:"booked",
+    tamilmonth:"sdnasj"
+});
+const defaultarray=[karthigaipoojadetail1,karthigaipoojadetail2,karthigaipoojadetail3];
+//karthigaipoojadetail.insertMany(defaultarray);
+
+const sastidetailschema={
+    date:String,
+    detail:String,
+    status:String,
+    tamilmonth:String
+};
+ const sastipoojadetail=mongoose.model("sastipoojadetail",sastidetailschema);
+ const sastipoojadetail1=new sastipoojadetail({
+     date:"03-06-2022",
+     detail:"gokul and gokul mobiles",
+     status:"booked",
+     tamilmonth:"sdnasj"
+ });
+ const sastipoojadetail2=new sastipoojadetail({
     date:2,
     detail:"praveen",
-    status:"booked"
+    status:"booked",
+    tamilmonth:"sdnasj"
 });
-const poojadetail3=new poojadetail({
+const sastipoojadetail3=new sastipoojadetail({
     date:3,
     detail:"Available",
-    status:"Available"
+    status:"Available",
+    tamilmonth:"sdnasj"
 });
-const defaultarray=[poojadetail1,poojadetail2,poojadetail3];
+const sastidefaultarray=[sastipoojadetail1,sastipoojadetail2,sastipoojadetail3];
+//sastipoojadetail.insertMany(sastidefaultarray);
 app.get("/",function(req,res){
     var fullday=currentdate.getDate();
     console.log(fullday);
@@ -40,8 +79,16 @@ app.get("/",function(req,res){
     {
         poojadetail.find({date:day},function(err,details)
         {
-            var name=details[0].detail;
-            res.render("poojadetail",{detail:founddetail,todaydetail:name,todaydate:fullday});
+            karthigaipoojadetail.find({},function(err,karthigai)
+            {
+                sastipoojadetail.find({},function(err,sasti)
+                {
+                    var name=details[0].detail;
+                    res.render("poojadetail",{detail:founddetail,todaydetail:name,todaydate:fullday,karthigai:karthigai,sasti:sasti});
+                });
+               
+            });
+            
         });
         //console.log(founddetail);
        
@@ -52,22 +99,53 @@ app.get("/",function(req,res){
   
 
 });
+app.post("/dailybookdetail",function(req,res){
+    const date=req.body.book;
+    console.log(date);
+    res.render("booking",{bookdate:date});
+    
+});
 app.post("/bookdetail",function(req,res){
     const date=req.body.book;
     console.log(date);
    
-    res.render("booking",{bookdate:date});
+    res.render("sastibooking",{bookdate:date});
     
   
 
 });
 const bookingschema={
-    date:Number,
+    date:String,
     detail:String,
     phoneNumber:Number
 };
 const booking=mongoose.model("booking",bookingschema);
 
+const sastibookingschema={
+    date:String,
+    detail:String,
+    phoneNumber:Number
+};
+const sastibooking=mongoose.model("sastibooking",sastibookingschema);
+
+app.post("/sastibooking",function(req,res)
+{
+    
+    
+    const date=req.body.date;
+    const detail=req.body.detail;
+    const phone=req.body.phone;
+    const book=new sastibooking({
+        date:date,
+        detail:detail,
+        phoneNumber:phone
+    });
+    book.save();
+
+res.redirect("/");
+
+
+});
 
 app.post("/booking",function(req,res)
 {
@@ -83,10 +161,12 @@ app.post("/booking",function(req,res)
     });
     book.save();
 
-res.redirect("/")
+res.redirect("/");
 
 
 });
+
+
 
 app.get("/adminlist",function(req,res)
 {
@@ -95,8 +175,15 @@ app.get("/adminlist",function(req,res)
       //  console.log(founddetail);
         booking.find({},function(err,bookingdetail)
         {
-            //console.log(founddetail);
-            res.render("adminlist",{booking:bookingdetail,detail:founddetail});
+            sastipoojadetail.find({},function(err,sasti)
+            {
+                sastibooking.find({},function(err,sastibook)
+                {
+                    res.render("adminlist",{booking:bookingdetail,detail:founddetail,sasti:sasti,sastibook:sastibook});
+                    //console.log(founddetail);
+                });
+
+            });
        
         });
     
@@ -111,18 +198,40 @@ app.post("/adminupdate",function(req,res)
     res.render("adminadd",{date:updatedate});
 
 });
+app.post("/sastiupdate",function(req,res)
+{
+    const updatedate=req.body.updatedate;
+    res.render("sastiadd",{date:updatedate});
+
+});
 app.post("/adminadd",function(req,res)
 {
     const date=req.body.date;
     const detail=req.body.detail;
     const status=req.body.status;
     console.log("date"+date+"detail"+detail+"status"+status);
-    const adminpoojadetail=new poojadetail({
-        date:date,
-        detail:detail,
-        status:status
-    });
+    
     poojadetail.updateOne({date:date},{detail:detail,status:status},function(err)
+    {
+        if(err)
+        {
+            console.log(err);
+        }else{
+            console.log("updated");
+        }
+
+    });
+    res.redirect("/adminlist");
+
+});
+app.post("/sastiadd",function(req,res)
+{
+    const date=req.body.date;
+    const detail=req.body.detail;
+    const status=req.body.status;
+    console.log("date"+date+"detail"+detail+"status"+status);
+  
+    sastipoojadetail.updateOne({date:date},{detail:detail,status:status},function(err)
     {
         if(err)
         {
@@ -181,6 +290,36 @@ app.post("/login",function(req,res)
   
 
 });
+app.post("/bookingdelete",function(req,res)
+{
+    const id=req.body.deleteid;
+    console.log(id);
+    booking.deleteOne({_id:id},function(err,res)
+    {
+        if(!err)
+        {
+            console.log("deleted");
+        }
+
+    });
+    res.redirect("/adminlist");
+
+});
+app.post("/sastibookdelete",function(req,res)
+{
+    const id=req.body.deleteid;
+    console.log(id);
+    sastibooking.deleteOne({_id:id},function(err,res)
+    {
+        if(!err)
+        {
+            console.log("deleted");
+        }
+
+    });
+    res.redirect("/adminlist");
+
+});
 app.get("/userlogin",function(req,res)
 {
 
@@ -194,6 +333,7 @@ app.get("/adminlogin",function(req,res)
 res.render("adminlogin",{});
 
 });
+
 app.listen(process.env.PORT || 5000,function()
 {
     console.log("server started");
